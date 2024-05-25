14ab6efc7dfaed4ff290709ced0d26394041962e
Muhannad Elbolaky <jmari@duck.com>
2024-05-26 00:55:40 +0300

    .env

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Panicf("error loading .env file: %v", err)
+		fmt.Printf("No .env file")
 	}
 
 	router := server.NewRouter()
